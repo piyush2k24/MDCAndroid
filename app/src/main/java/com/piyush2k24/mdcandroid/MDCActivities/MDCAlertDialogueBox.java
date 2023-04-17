4ec455782eb1e2547a8564e8b8aab16e18e5a3ae
Piyush Makwana <piyush2k24@gmail.com>
2023-04-17 13:43:43 +0530

    All Working Ok

--- app/src/main/java/com/piyush2k24/mdcandroid/MDCActivities/MDCAlertDialogueBox.java
+++ app/src/main/java/com/piyush2k24/mdcandroid/MDCActivities/MDCAlertDialogueBox.java
@@ -2,15 +2,64 @@ package com.piyush2k24.mdcandroid.MDCActivities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 import com.piyush2k24.mdcandroid.R;
+import com.piyush2k24.mdcandroid.databinding.MdcAlertDialogueBoxBinding;
 
 public class MDCAlertDialogueBox extends AppCompatActivity {
-
+MdcAlertDialogueBoxBinding binding;
+Intent intent;
+MaterialAlertDialogBuilder materialAlertDialogBuilder;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.mdc_alert_dialogue_box);
+        binding=MdcAlertDialogueBoxBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        Caller();
+    }
+
+    private void Caller() {
+        binding.MDC3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                materialAlertDialogBuilder= new MaterialAlertDialogBuilder(MDCAlertDialogueBox.this)
+                        .setIcon(R.mipmap.ic_launcher)
+                        .setMessage("This is the material dialogue box")
+                        .setTitle("MDC-AlertDialogueBox")
+                        .setCancelable(false)
+                        .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                showToast("MDC Clicked ! Working");
+                            }
+                        })
+                        .setNegativeButton("Na", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                                showToast("Dismiss 👍");
+                            }
+                        });
+                        materialAlertDialogBuilder.create().show();
+            }
+        });
+        binding.btnSourceCode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                intent= new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/material-components/material-components-android/blob/master/docs/components/Dialog.md"));
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void showToast(String str) {
+        Toast.makeText(this, str, Toast.LENGTH_SHORT).show();
     }
 }
--- app/src/main/java/com/piyush2k24/mdcandroid/MDCActivities/MDCDatePicker.java
+++ app/src/main/java/com/piyush2k24/mdcandroid/MDCActivities/MDCDatePicker.java
@@ -2,15 +2,52 @@ package com.piyush2k24.mdcandroid.MDCActivities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
+import com.google.android.material.datepicker.MaterialDatePicker;
+import com.google.android.material.datepicker.MaterialPickerOnPositiveButtonClickListener;
+import com.piyush2k24.mdcandroid.MDCAdapter;
 import com.piyush2k24.mdcandroid.R;
+import com.piyush2k24.mdcandroid.databinding.MdcDatepickerBinding;
 
 public class MDCDatePicker extends AppCompatActivity {
-
+MdcDatepickerBinding binding;
+Intent intent;
+MaterialDatePicker<Long> materialDatePicker;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.mdc_datepicker);
+        binding= MdcDatepickerBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        Callers();
+    }
+
+    private void Callers() {
+        binding.MDC2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                materialDatePicker=MaterialDatePicker.Builder.datePicker()
+                        .setTitleText("Choose Your date")
+                        .setSelection(MaterialDatePicker.todayInUtcMilliseconds())
+                        .build();
+                materialDatePicker.show(getSupportFragmentManager(),"MDC");
+                materialDatePicker.addOnPositiveButtonClickListener(new MaterialPickerOnPositiveButtonClickListener<Long>() {
+                    @Override
+                    public void onPositiveButtonClick(Long selection) {
+                        binding.MDC2.setText(materialDatePicker.getHeaderText());
+                    }
+                });
+            }
+        });
+        binding.btnSourceCode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                intent=new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/material-components/material-components-android/blob/master/docs/components/DatePicker.md"));
+                startActivity(intent);
+            }
+        });
     }
 }
